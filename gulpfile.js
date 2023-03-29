bb67d4431fff4fa7098c8a209cdf8aed22c32ed4
Paola Villalobos <yulian_yuratma0328@hotmail.com>
2023-03-28 22:02:20 -0500

    extensiones avif, webp convertirlas con gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,15 @@
-const { src, dest, watch } = require("gulp");  //requiere es un forma de extraer el gulp
+const { src, dest, watch, parallel } = require("gulp");  //requiere es un forma de extraer el gulp
+
+//Css
 const sass = require("gulp-sass") (require('sass'));
 const plumber = require('gulp-plumber');
+
+
+//Imagenes
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
+const avif = require('gulp-avif');
 
 function css (done){
 
@@ -12,12 +21,48 @@ function css (done){
     done (); //callback que avisa a gulp cuando llegamos al final
 }
 
-function dev(done) {
+function imagenes (done) {
+    const opciones = {
+        optimizationLevel: 3
+    }
+    src ('src/img/**/*.{png,jpg}')
+        .pipe (cache ( imagemin(opciones)) )
+        .pipe (dest ('build/img') )
+    done ();
+}
+function versionWebp ( done ) {
 
-    watch("src/scss/**/*.scss", css)
+    const opciones = {
+        quality: 50
+    };
 
+
+    src ('src/img/**/*.{png,jpg}')
+        .pipe( webp (opciones) )
+        .pipe( dest ('build/img') )
+    done ();
+}
+
+function versionAvif( done ) {
+
+    const opciones = {
+        quality: 50
+    };
+
+
+    src ('src/img/**/*.{png,jpg}')
+        .pipe( avif (opciones) )
+        .pipe( dest ('build/img') )
+    done ();
+}
+
+function dev( done ) {
+    watch('src/scss/**/*.scss', css);
     done();
 }
 
 exports.css = css;
-exports.dev = dev;
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel ( imagenes, versionWebp, versionAvif, dev) ;
